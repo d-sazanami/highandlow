83665859fcd0087ecaecc71d99d73841ffbf8708
sazanami <dstrsazanami@gmail.com>
2021-10-02 11:36:38 +0900

    初期表示処理作成

--- app/src/main/java/com/example/sazanami/highandlow/MainActivity.kt
+++ app/src/main/java/com/example/sazanami/highandlow/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.sazanami.highandlow
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
@@ -10,49 +11,60 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
 import com.example.sazanami.highandlow.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
+    private val tag = "high and low"
+    private var yourCard = 0
+    private var droidCard = 0
+    private var hitCount = 0
+    private var loseCount = 0
+    private var gameStart = false
+    private var answered = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+    }
 
-        setSupportActionBar(binding.toolbar)
+    override fun onResume() {
+        super.onResume()
+        hitCount = 0
+        loseCount = 0
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        hitText.text = getString(R.string.hit_text)
+        loseText.text = getString(R.string.lose_text)
+        gameStart = true
+        drawCard()
+    }
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+    private fun drawCard() {
+        yourCardImage.setImageResource(R.drawable.bk0)
+        droidCardImage.setImageResource(R.drawable.bk0)
+
+        yourCard = (1..13).random()
+        Log.d(tag, "You: " + yourCard)
+        when(yourCard) {
+            1 -> yourCardImage.setImageResource(R.drawable.c01)
+            2 -> yourCardImage.setImageResource(R.drawable.c02)
+            3 -> yourCardImage.setImageResource(R.drawable.c03)
+            4 -> yourCardImage.setImageResource(R.drawable.c04)
+            5 -> yourCardImage.setImageResource(R.drawable.c05)
+            6 -> yourCardImage.setImageResource(R.drawable.c06)
+            7 -> yourCardImage.setImageResource(R.drawable.c07)
+            8 -> yourCardImage.setImageResource(R.drawable.c08)
+            9 -> yourCardImage.setImageResource(R.drawable.c09)
+            10 -> yourCardImage.setImageResource(R.drawable.c10)
+            11 -> yourCardImage.setImageResource(R.drawable.c11)
+            12 -> yourCardImage.setImageResource(R.drawable.c12)
+            13 -> yourCardImage.setImageResource(R.drawable.c13)
         }
-    }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+        droidCard = (1..13).random()
+        Log.d(tag, "droid : " + droidCard)
+        answered = false
     }
 }
