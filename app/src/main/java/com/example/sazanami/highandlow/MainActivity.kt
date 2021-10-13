36a92ca4628612714f8ebf57afbe8cd91d9b476c
sazanami <dstrsazanami@gmail.com>
2021-10-13 20:18:51 +0900

    ビューバインディングで実装

--- app/src/main/java/com/example/sazanami/highandlow/MainActivity.kt
+++ app/src/main/java/com/example/sazanami/highandlow/MainActivity.kt
@@ -11,7 +11,6 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
 import com.example.sazanami.highandlow.databinding.ActivityMainBinding
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
@@ -23,11 +22,31 @@ class MainActivity : AppCompatActivity() {
     private var loseCount = 0
     private var gameStart = false
     private var answered = false
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
+        binding.hightBtn.setOnClickListener{
+            if ((gameStart && !answered)) {
+                highAndLow('h')
+            }
+        }
+
+        binding.lowBtn.setOnClickListener {
+            if ((gameStart && !answered)) {
+                highAndLow('l')
+            }
+        }
+
+        binding.nextBtn.setOnClickListener{
+            if (gameStart) {
+                drawCard()
+            }
+        }
     }
 
     override fun onResume() {
@@ -35,36 +54,80 @@ class MainActivity : AppCompatActivity() {
         hitCount = 0
         loseCount = 0
 
-        hitText.text = getString(R.string.hit_text)
-        loseText.text = getString(R.string.lose_text)
+        binding.hitText.text = getString(R.string.hit_text)
+        binding.loseText.text = getString(R.string.lose_text)
         gameStart = true
         drawCard()
     }
 
     private fun drawCard() {
-        yourCardImage.setImageResource(R.drawable.bk0)
-        droidCardImage.setImageResource(R.drawable.bk0)
+        binding.yourCardImage.setImageResource(R.drawable.bk0)
+        binding.droidCardImage.setImageResource(R.drawable.bk0)
 
         yourCard = (1..13).random()
         Log.d(tag, "You: " + yourCard)
         when(yourCard) {
-            1 -> yourCardImage.setImageResource(R.drawable.c01)
-            2 -> yourCardImage.setImageResource(R.drawable.c02)
-            3 -> yourCardImage.setImageResource(R.drawable.c03)
-            4 -> yourCardImage.setImageResource(R.drawable.c04)
-            5 -> yourCardImage.setImageResource(R.drawable.c05)
-            6 -> yourCardImage.setImageResource(R.drawable.c06)
-            7 -> yourCardImage.setImageResource(R.drawable.c07)
-            8 -> yourCardImage.setImageResource(R.drawable.c08)
-            9 -> yourCardImage.setImageResource(R.drawable.c09)
-            10 -> yourCardImage.setImageResource(R.drawable.c10)
-            11 -> yourCardImage.setImageResource(R.drawable.c11)
-            12 -> yourCardImage.setImageResource(R.drawable.c12)
-            13 -> yourCardImage.setImageResource(R.drawable.c13)
+            1 -> binding.yourCardImage.setImageResource(R.drawable.c01)
+            2 -> binding.yourCardImage.setImageResource(R.drawable.c02)
+            3 -> binding.yourCardImage.setImageResource(R.drawable.c03)
+            4 -> binding.yourCardImage.setImageResource(R.drawable.c04)
+            5 -> binding.yourCardImage.setImageResource(R.drawable.c05)
+            6 -> binding.yourCardImage.setImageResource(R.drawable.c06)
+            7 -> binding.yourCardImage.setImageResource(R.drawable.c07)
+            8 -> binding.yourCardImage.setImageResource(R.drawable.c08)
+            9 -> binding.yourCardImage.setImageResource(R.drawable.c09)
+            10 -> binding.yourCardImage.setImageResource(R.drawable.c10)
+            11 -> binding.yourCardImage.setImageResource(R.drawable.c11)
+            12 -> binding.yourCardImage.setImageResource(R.drawable.c12)
+            13 -> binding.yourCardImage.setImageResource(R.drawable.c13)
         }
 
         droidCard = (1..13).random()
         Log.d(tag, "droid : " + droidCard)
         answered = false
+    }
+
+    private fun highAndLow(answer:Char) {
+        showDroidCard()
+        answered = true
+        val balance = droidCard - yourCard
+        if (balance == 0) {
+        } else if ((balance > 0 && answer == 'h')) {
+            hitCount++
+            binding.hitText.text = getString(R.string.hit_text) + hitCount
+        } else if ((balance < 0 && answer == 'l')) {
+            hitCount++
+            binding.hitText.text = getString(R.string.hit_text) + hitCount
+        } else {
+            loseCount++
+            binding.loseText.text = getString(R.string.lose_text)  + loseCount
+        }
+
+        if (hitCount == 5) {
+            binding.resultText.text = "あなたの勝ちです"
+            gameStart = false
+        } else if (loseCount == 5) {
+            binding.resultText.text = "あなたの負けです"
+            gameStart = false
+        } else {
+        }
+    }
+
+    private fun showDroidCard() {
+        when (droidCard) {
+            1 -> binding.droidCardImage.setImageResource(R.drawable.c01)
+            2 -> binding.droidCardImage.setImageResource(R.drawable.c02)
+            3 -> binding.droidCardImage.setImageResource(R.drawable.c03)
+            4 -> binding.droidCardImage.setImageResource(R.drawable.c04)
+            5 -> binding.droidCardImage.setImageResource(R.drawable.c05)
+            6 -> binding.droidCardImage.setImageResource(R.drawable.c06)
+            7 -> binding.droidCardImage.setImageResource(R.drawable.c07)
+            8 -> binding.droidCardImage.setImageResource(R.drawable.c08)
+            9 -> binding.droidCardImage.setImageResource(R.drawable.c09)
+            10 -> binding.droidCardImage.setImageResource(R.drawable.c10)
+            11 -> binding.droidCardImage.setImageResource(R.drawable.c11)
+            12 -> binding.droidCardImage.setImageResource(R.drawable.c12)
+            13 -> binding.droidCardImage.setImageResource(R.drawable.c13)
+        }
     }
 }
